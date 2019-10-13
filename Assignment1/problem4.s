11832a112b65e8c89afcf4faa35dba0272daef8c
Ahmet Uysal <auysal16@ku.edu.tr>
2019-10-14 02:50:30 +0300

    Updated A1 P4 first implementation to use half-word length memory ops

--- Assignment1/problem4.s
+++ Assignment1/problem4.s
@@ -16,8 +16,8 @@ addi	$t0, $zero, 0			    # temp0 = 0 + 0, keeps current iteration number
 
 Loop: beq		$t0, $s1, Exit	# if temp0 (current iteration) == CNT jump to Exit
 
-lw		$t2, 0($s2)		        # Load 4 bytes of SRC to temp2 
-sw		$t2, 0($s3)		        # Save temp2 to DST (saves 4 bytes, 2 bytes will be overriden at next iteration)
+lh		$t2, 0($s2)		        # Load half-word (2 bytes) of SRC to temp2 
+sh		$t2, 0($s3)		        # Save temp2 half-word to DST (saves 2 bytes)
 addi	$s2, $s2, 2			      # SRC = SRC + 2
 addi	$s3, $s3, 2			      # DST = DST + 2
 addi	$t0, $t0, 1			      # temp0++, increment iteration counter by 1
