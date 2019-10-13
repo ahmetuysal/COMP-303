e0fa5c3b7ddf45c7b9cba920f19c5f52671f5954
Ahmet Uysal <auysal16@ku.edu.tr>
2019-10-14 01:29:14 +0300

    A1.p4 bugfix and added an alternative implementation

--- Assignment1/problem4.s
+++ Assignment1/problem4.s
@@ -12,15 +12,32 @@
 # *Idea 1: Move each short integer (2 bytes) separately
 
 # Use a register to store current iteration
-addi	$t0, $zero 0			    # temp0 = 0 + 0, keeps current iteration number
-addi	$t1, $s1, -1			    # temp1 = CNT - 1, used in stop condition 
-Loop: beq		$t0, $t1, Exit	# if temp0 (current iteration) == temp1 (no. iterations - 1) jump to Exit
+addi	$t0, $zero, 0			    # temp0 = 0 + 0, keeps current iteration number
+
+Loop: beq		$t0, $s1, Exit	# if temp0 (current iteration) == CNT jump to Exit
+
 lw		$t2, 0($s2)		        # Load 4 bytes of SRC to temp2 
 sw		$t2, 0($s3)		        # Save temp2 to DST (saves 4 bytes, 2 bytes will be overriden at next iteration)
 addi	$s2, $s2, 2			      # SRC = SRC + 2
 addi	$s3, $s3, 2			      # DST = DST + 2
-addi	$t0, $t0, 1			      # temp0++, increment iteration counter
+addi	$t0, $t0, 1			      # temp0++, increment iteration counter by 1
+
 j Loop
 Exit: ...
 
+# *Idea 2: Move two short integers at each iteration
+
+# Use a register to store current iteration
+addi	$t0, $zero, 0			    # temp0 = 0 + 0, keeps current iteration number
+# Need to use >= as it it can get CNT + 1 since we are incrementing by 2
+Loop: bge		$t0, $s1, Exit	# if temp0 (current iteration) >= CNT then jump to Exit
+
+lw		$t2, 0($s2)		        # Load 4 bytes of SRC to temp2 
+sw		$t2, 0($s3)		        # Save temp2 to DST
+addi	$s2, $s2, 4			      # SRC = SRC + 4
+addi	$s3, $s3, 4			      # DST = DST + 4
+addi	$t0, $t0, 2			      # temp0 = temp0 + 2, increment iteration counter by 2
+
+j Loop
+Exit: ...
 
