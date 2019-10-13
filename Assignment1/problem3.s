fa9bafc4645416ad1999fa09f8041402658de823
Ahmet Uysal <auysal16@ku.edu.tr>
2019-10-14 02:47:17 +0300

    Added A1 p3 MIPS implementation

--- Assignment1/problem3.s
+++ Assignment1/problem3.s
@@ -13,8 +13,14 @@ POP   $s0   # Pop X and save it back to $s0, Stack: [
 PUSH  $s0   # Push X to stack, Stack: [X
 PUSH  $s2   # Push Z to stack, Stack: [X, Z
 ADD         # Pop two values, push val1 + val2 back to stack, Stack: [Y(X + Z)
-POP   $s0   # Pop Y and save it back to $s1, Stack: [
+POP   $s1   # Pop Y and save it back to $s1, Stack: [
 PUSH  $s0   # Push X to stack, Stack: [X
 PUSH  $s1   # Push Y to stack, Stack: [X, Y
 SUB         # Pop two values, push val1 - val2 back to stack, Stack: [T(X - Y)
 POP   $s3   # Pop Z and save it back to $s3, Stack: [
+
+# MIPS implementation
+# Assumed X, Y, Z, T stored in $s0, $s1, $s2, $s3, respectively
+add		$s0, $s1, $s2		# X = Y + Z
+add		$s1, $s0, $s2		# Y = X + Z
+sub		$s3, $s0, $s1		# T = X - Y
