3e547ed65f54faeddc7df08f1b182ab09ad30d19
Ahmet Uysal <auysal16@ku.edu.tr>
2019-10-14 04:11:47 +0300

    A1 P3 added save and load instructions to MIPS implementation

--- Assignment1/problem3.s
+++ Assignment1/problem3.s
@@ -20,7 +20,12 @@ SUB         # Pop two values, push val1 - val2 back to stack, Stack: [T(X - Y)
 POP   $s3   # Pop Z and save it back to $s3, Stack: [
 
 # MIPS implementation
-# Assumed X, Y, Z, T stored in $s0, $s1, $s2, $s3, respectively
-add		$s0, $s1, $s2		# X = Y + Z
-add		$s1, $s0, $s2		# Y = X + Z
-sub		$s3, $s0, $s1		# T = X - Y
+# Assumed addresses of X, Y, Z, T are stored in $s0, $s1, $s2, $s3, respectively
+lw		$t1, 0($s1)		  # Load value of Y to temp1
+lw		$t2, 0($s2)		  # Load value of Z to temp2
+add		$t0, $t1, $t2		# X = Y + Z
+sw		$t0, 0($s0)		  # Save updated value of X 
+add		$t1, $t0, $t2		# Y = X + Z
+sw		$t1, 0($s1)		  # Save updated value of Y
+sub		$t3, $t0, $t1		# T = X - Y
+sw    $t3, 0($s3)     # Save updated value of T
